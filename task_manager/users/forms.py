80e40cd784f532db7c55f71eca8013f0eb281740
LikerK <nagorniykirill@gmail.com>
2022-06-21 10:27:36 +0400

    refactoring

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -1,11 +1,10 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
-from .models import User
 
 
 class UserForm(UserCreationForm):
     class Meta:
-        user = get_user_model()
+        User = get_user_model()
         model = User
         fields = [
             'first_name',
